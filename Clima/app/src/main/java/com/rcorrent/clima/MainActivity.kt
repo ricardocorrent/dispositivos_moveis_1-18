561a551f9366ef8a68c198e34f0899706d4f6e56
Ricardo Corrent <ricardocorrent@hotmail.com>
2018-04-05 10:40:27 -0300

    clima

--- Clima/app/src/main/java/com/rcorrent/clima/MainActivity.kt
+++ Clima/app/src/main/java/com/rcorrent/clima/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.rcorrent.clima
 
+import android.os.AsyncTask
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.Toast
+import com.rcorrent.buscadorgithub.NetworkUtils
 import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,9 +32,97 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        for(i in dadosClimaFicticios){
-            dados_clima.append("$i \n\n\n")
-        }
+        Toast.makeText(this, "Criado", Toast.LENGTH_SHORT).show()
+//        for(i in dadosClimaFicticios){
+//            dados_clima.append("$i \n\n\n")
+//        }
+        carregarDadosDoClima()
     }
+
+    fun carregarDadosDoClima(){
+        val localizacao = ClimaPreferencias.getLocalizacaoSalva(this)
+        BuscarClimaTask().execute(localizacao)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.clima, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        if(item?.itemId == R.id.acao_atualizar){
+            dados_clima.text = ""
+            Toast.makeText(this, "onOption", Toast.LENGTH_SHORT).show()
+            carregarDadosDoClima()
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    fun exibirResultado(){
+        dados_clima.visibility = View.VISIBLE
+        tv_mensagem_erro.visibility = View.INVISIBLE
+        pb_aguarde.visibility = View.INVISIBLE
+    }
+
+    fun exibirMensagemErro(){
+        dados_clima.visibility = View.INVISIBLE
+        tv_mensagem_erro.visibility = View.VISIBLE
+        pb_aguarde.visibility = View.INVISIBLE
+    }
+
+    fun exibirProgressBar(){
+        dados_clima.visibility = View.INVISIBLE
+        tv_mensagem_erro.visibility = View.INVISIBLE
+        pb_aguarde.visibility = View.VISIBLE
+    }
+
+
+
+
+
+    inner class BuscarClimaTask(): AsyncTask<String, Void, String>(){
+
+        override fun onPreExecute() {
+            exibirProgressBar()
+        }
+
+        override fun doInBackground(vararg params: String): String? {
+            try {
+                val localizacao = params[0];
+                val url = NetworkUtils.construirUrl(localizacao)
+
+                if (url != null){
+                    val resultado = NetworkUtils.obterRespostaDaUrlHttp(url)
+                    return resultado
+                }
+            } catch (e: Exception){
+                e.printStackTrace()
+            }
+            return null
+        }
+
+        override fun onPostExecute(result: String?) {
+
+            if(result != null){
+
+                val jsonResultado = JSONObject(result)
+                val jsonDados = jsonResultado.getJSONArray("")
+                dados_clima.text = "teste"
+                dados_clima.text = result
+
+                exibirResultado()
+            }else{
+                dados_clima.text = "mensagem de erro!!"
+                exibirMensagemErro()
+            }
+
+        }
+
+    }
+
+
+
+
+
+
 }
